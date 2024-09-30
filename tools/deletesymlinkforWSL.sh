c9d3fd35d86ba53287a830171b99893f4c577835
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2024-09-30 03:14:43 +0300

    Create deletesymlinkforWSL.sh

---
+++ tools/deletesymlinkforWSL.sh
@@ -0,0 +1,14 @@
+sudo rm /usr/bin/nmap
+sudo rm /usr/bin/ncat
+sudo rm /usr/bin/amass
+sudo rm /usr/bin/hydra
+sudo rm /usr/bin/rustscan
+sudo rm /usr/bin/ffuf
+sudo rm /usr/bin/subfinder
+sudo rm /usr/bin/httpx
+sudo rm /usr/bin/nuclei
+sudo rm /usr/bin/vaf
+sudo rm /usr/bin/webanalyze
+sudo rm /usr/bin/s3scanner
+sudo rm /usr/bin/gau
+sudo rm /usr/bin/qsreplace
